7f54149bc70ffa916deb3d90a17602d585fa9f0d
Abderra <66704312+abdedarghal111@users.noreply.github.com>
2025-07-16 18:43:14 +0200

    Refine channel description and tweak homepage text

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Soy Abderra';
+export const SITE_DESCRIPTION =
+  'Subo contenido de Roblox y, a veces, de programación o Roblox Studio. Aquí encontrarás recursos, anuncios y documentación.';
